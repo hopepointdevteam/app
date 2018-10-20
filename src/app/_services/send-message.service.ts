baeb1d5ea902add69988b9fbc3de1722e7e669f6
Scott Capps <capps.scott@gmail.com>
2018-10-20 16:21:26 -0700

    small bug fix

--- src/app/_services/send-message.service.ts
+++ src/app/_services/send-message.service.ts
@@ -1,5 +1,5 @@
 import { WINDOW } from '@ng-toolkit/universal';
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Message } from '../../../build/models/message';
